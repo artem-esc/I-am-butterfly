907ad45f7ce1d9b14923ed9d50444868184faa71
ESCG\535439 <535439@chrome.escg.ac.uk>
2022-09-27 17:02:23 +0100

    main

--- i-am-butterfly/Program.cs
+++ i-am-butterfly/Program.cs
@@ -388,6 +388,8 @@ class Program
 
         // https://manytools.org/hacker-tools/convert-images-to-ascii-art/go/
         string[] butterfly_frames = new string[] {_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,_18};
+        Console.Title = "I am butterfly";
+        Console.SetWindowSize(120, 23);
         int t = 0;
         string a = " ";
         while (true)
@@ -396,7 +398,7 @@ class Program
             foreach (string butterfly_frame in butterfly_frames)
             {
 
-                Console.WriteLine(a + butterfly_frame);
+                Console.WriteLine(butterfly_frame);
                 Thread.Sleep(50);
 
             }
